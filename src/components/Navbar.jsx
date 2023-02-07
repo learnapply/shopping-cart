340385f2154623150fbc169ab3f1f45c9412db5d
abydenes <108868128+abydenes@users.noreply.github.com>
2023-02-07 20:45:28 +0300

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='navbar-container'> 
+      <h1>Shopping Cart</h1>
+      <div className='navbar-buttons-container'>
+        <button>Home</button>
+        <button>Shop</button>
+        <button>Cart</button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
