93bb607aa0085d197eec1e69c4792f582c79e9f1
abydenes <108868128+abydenes@users.noreply.github.com>
2023-02-07 21:22:24 +0300

    Add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import "./App.css";
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import Cart from "./components/Cart";
+import Shop from "./components/Shop";
+import "./App.css";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-      <Home />
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div className='navbar-container'> 
+    <div className="navbar-container">
       <h1>Shopping Cart</h1>
-      <div className='navbar-buttons-container'>
-        <button>Home</button>
-        <button>Shop</button>
-        <button>Cart</button>
+      <div className="navbar-buttons-container">
+        <Link to="/">
+          <button>Home</button>
+        </Link>
+        <Link to="/shop">
+          <button>Shop</button>
+        </Link>
+        <Link to="/cart">
+          <button>Cart</button>
+        </Link>
       </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
