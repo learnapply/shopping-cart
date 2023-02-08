fc4ad26e39f1a1b39e1b08d04ba99a2e22c620ae
abydenes <108868128+abydenes@users.noreply.github.com>
2023-02-08 12:53:28 +0300

    Add basic data for products

---
+++ src/data/products.js
@@ -0,0 +1,52 @@
+import frankZhang from "../assets/frank-zhang.jpg";
+import joeGreen from "../assets/joe-green.jpg";
+import mcheLee from "../assets/mche-lee.jpg";
+import rodrigoDosReis from "../assets/rodrigo-dos-reis.jpg";
+import rosannaGaddoni from "../assets/rosanna-gaddoni.jpg";
+import shelleyPauls from "../assets/shelley-pauls.jpg";
+import xuanNguyen from "../assets/xuan-nguyen";
+
+export const products = [
+  {
+    id: 0,
+    name: "small garlic",
+    desc: "Ut id sagittis sem. Suspendisse aliquam bibendum nunc, nec elementum nunc tempor eget. Vestibulum sed aliquet ligula. Nulla sit amet.",
+    src: frankZhang,
+  },
+  {
+    id: 1,
+    name: "purple garlic",
+    desc: "Aenean et mi fermentum, tincidunt ligula vitae, vehicula felis. Duis et mauris sapien. Ut ut nisi vel lorem dapibus facilisis.",
+    src: joeGreen,
+  },
+  {
+    id: 2,
+    name: "white garlic",
+    desc: "Pellentesque metus ligula, finibus vel vestibulum ut, commodo eget tellus. Morbi tristique sodales tortor at imperdiet. Morbi tempus mauris quis.",
+    src: mcheLee,
+  },
+  {
+    id: 3,
+    name: "red garlic heads",
+    desc: "In interdum diam ipsum, ut ullamcorper metus varius nec. Aliquam erat volutpat. Maecenas vehicula augue et viverra hendrerit. Integer ex.",
+    src: rodrigoDosReis,
+  },
+  {
+    id: 4,
+    name: "wall garlic",
+    desc: "In cursus porttitor magna, sit amet aliquam arcu scelerisque sed. Cras magna dui, facilisis congue euismod faucibus, ultrices bibendum nulla.",
+    src: rosannaGaddoni,
+  },
+  {
+    id: 5,
+    name: "fresh garlic",
+    desc: "Proin ac nisi vitae lorem lacinia iaculis ut non magna. Curabitur iaculis quis dui in lobortis. Nunc mattis ut dolor.",
+    src: shelleyPauls,
+  },
+  {
+    id: 6,
+    name: "purple garlic heads",
+    desc: "Morbi molestie odio ac ante feugiat cursus. Fusce pretium ipsum id ex consequat consectetur. Vivamus tincidunt eu justo a efficitur.",
+    src: xuanNguyen,
+  },
+];
